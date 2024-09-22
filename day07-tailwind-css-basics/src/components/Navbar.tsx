e02bb0fc756ba60aaee7adbd9a2fe349e90736b2
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-09-22 21:30:39 +0500

    updated

--- day07-tailwind-css-basics/src/components/Navbar.tsx
+++ day07-tailwind-css-basics/src/components/Navbar.tsx
@@ -1,10 +1,51 @@
 import Link from "next/link";
 
 export default function Navbar() {
-  <div>
-    <nav>
-      <div>left</div>
-      <div>left</div>
-    </nav>
-  </div>;
+  return (
+    <div>
+      <nav className=" flex justify-between items-center px-4 w-100% bg-blue-800 sm:px-10">
+        <Link href="/" className="text-2xl py-6 font-semibold text-white">
+          Zubair AR
+        </Link>
+        <div>
+          <Link
+            href="/"
+            className="uppercase text-white text-xs tracking-wide mx-1.5 hover:border-b-2"
+          >
+            Home
+          </Link>
+          <Link
+            href="/"
+            className="uppercase text-white text-xs tracking-wide mx-1.5 hover:border-b-2"
+          >
+            About
+          </Link>
+          <Link
+            href="/"
+            className="uppercase text-white text-xs tracking-wide mx-1.5 hover:border-b-2"
+          >
+            Career
+          </Link>
+          <Link
+            href="/"
+            className="uppercase text-white text-xs tracking-wide mx-1.5 hover:border-b-2"
+          >
+            Skills
+          </Link>
+          <Link
+            href="/"
+            className="uppercase text-white text-xs tracking-wide mx-1.5 hover:border-b-2"
+          >
+            Portfolio
+          </Link>
+          <Link
+            href="/"
+            className="uppercase text-white text-xs tracking-wide mx-1.5 hover:border-b-2"
+          >
+            COntact US
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
 }

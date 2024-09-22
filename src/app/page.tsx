ce33d536331124f5a7f482f08510cea9064e56b2
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-09-22 14:12:17 +0500

    updated

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="min-w-8 bg-red-700 text-center m-36 p-20 rounded-xl ">
+      <h1 className="p-7 text-3xl leading-loose font-sm text-white">
+        Install
+        <span className="text-5xl text-yellow-400 font-bold uppercase tracking-widest">
+          "Tailwind CSS"
+        </span>
+        setting Environment
+      </h1>
+    </div>
+  );
+}

55dfe38e5b0ce82a882076a528022e5eab700a4d
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-09-23 15:31:15 +0500

    update

--- day07-tailwind-css-basics/src/components/Navbar.tsx
+++ day07-tailwind-css-basics/src/components/Navbar.tsx
@@ -1,15 +1,37 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 import { CgMenuRight } from "react-icons/cg";
-import { DiVim } from "react-icons/di";
 
 export default function Navbar() {
+  let [isOpen, setIsOpen] = useState(false);
+
+  function getMenuClasses() {
+    let menuClasses = [];
+    if (isOpen) {
+      menuClasses = [
+        "flex",
+        "absolute",
+        "top-[60px]",
+        "bg-gray-800",
+        "w-full",
+        "p-4",
+        "left-0",
+        "gap-10",
+        "flex-col",
+      ];
+    } else {
+      menuClasses = ["hidden", "md:flex", "md:gap-10"];
+    }
+    return menuClasses.join(" ");
+  }
   return (
     <div>
       <nav className=" flex justify-between items-center px-4 w-100% bg-blue-800 sm:px-10">
         <Link href="/" className="text-2xl py-6 font-semibold text-white">
           Zubair AR
         </Link>
-        <div className="hidden md:block">
+        <div className={getMenuClasses()}>
           <Link
             href="/"
             className="uppercase text-white text-xs tracking-wide mx-1.5 hover:border-b-2"
@@ -47,7 +69,15 @@ export default function Navbar() {
             COntact US
           </Link>
         </div>
-        <CgMenuRight className=" text-white text-2xl block md:hidden" />
+        <div className="md:hidden flex items-center">
+          <button
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            <CgMenuRight className=" text-white text-2xl block md:hidden cursor-pointer" />
+          </button>
+        </div>
       </nav>
     </div>
   );

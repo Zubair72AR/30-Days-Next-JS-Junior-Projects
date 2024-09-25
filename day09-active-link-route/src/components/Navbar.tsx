90e133c59893b5867a4a924100d174a8125547d3
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-09-25 19:50:38 +0500

    updated

--- day09-active-link-route/src/components/Navbar.tsx
+++ day09-active-link-route/src/components/Navbar.tsx
@@ -1,22 +1,17 @@
+"use client";
 import Link from "next/link";
-import Image from "next/image";
 import { useRouter } from "next/router";
 
 export default function Navbar() {
-  let router = useRouter();
-  console.log(router);
+  const router = useRouter();
+  const isActive = (href) => router.pathname === href;
   return (
     <nav>
-      <Image
-        src="https://nextjs.org/icons/next.svg"
-        alt="Next.js logo"
-        width={100}
-        height={38}
-        priority
-      />
       <ul>
         <li>
-          <Link href=""></Link>
+          <Link href="/" className={isActive("/") ? "active" : ""}>
+            Home
+          </Link>
         </li>
       </ul>
     </nav>

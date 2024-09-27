f582256329f9b2e16eb8e26a3385b7cdca2b8ac8
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-09-27 17:15:38 +0500

    updated

--- day10-form-input-handling/src/components/form.tsx
+++ day10-form-input-handling/src/components/form.tsx
@@ -8,9 +8,13 @@ interface Submission {
 }
 
 export default function Form() {
-  let [name, setName] = useState("");
-  let [email, SetEmail] = useState("");
-  let [message, SetMessage] = useState("");
+  let [name, setName] = useState<string>("");
+  let [email, setEmail] = useState<string>("");
+  let [message, setMessage] = useState<string>("");
+  let [isNameLength, setIsNameLength] = useState<boolean>(true);
+  let [isEmailLength, setIsEmailLength] = useState<boolean>(true);
+  let [isMessageLength, setIsMessageLength] = useState<boolean>(true);
+
   let handleForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -19,6 +23,39 @@ export default function Form() {
       email,
       message,
     };
+
+    console.log(submission);
+  };
+
+  const handleName = (e: ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value;
+    setName(value);
+
+    if (value.length < 4) {
+      setIsNameLength(false);
+    } else {
+      setIsNameLength(true);
+    }
+  };
+
+  const handleEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value;
+    setEmail(value);
+    if (value.length < 5) {
+      setIsEmailLength(false);
+    } else {
+      setIsEmailLength(true);
+    }
+  };
+
+  const handleMessage = (e: ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value;
+    setMessage(value);
+    if (value.length < 10) {
+      setIsMessageLength(false);
+    } else {
+      setIsMessageLength(true);
+    }
   };
 
   return (
@@ -30,9 +67,12 @@ export default function Form() {
           </label>
           <input
             type="text"
+            name="name"
             placeholder="Enter here"
-            onChange={(e) => setName(e.target.value)}
+            required
+            onChange={handleName}
           />
+          {!isNameLength && <p>Minimum length required is 4</p>}
         </div>
         <div>
           <label>
@@ -40,9 +80,12 @@ export default function Form() {
           </label>
           <input
             type="text"
+            name="email"
             placeholder="Enter here"
-            onChange={(e) => SetEmail(e.target.value)}
+            required
+            onChange={handleEmail}
           />
+          {!isEmailLength && <p>Minimum length required is 4</p>}
         </div>
         <div>
           <label>
@@ -50,13 +93,15 @@ export default function Form() {
           </label>
           <textarea
             placeholder="Enter here"
+            required
             rows={4}
-            onChange={(e) => SetMessage(e.target.value)}
+            onChange={handleMessage}
           />
+          {!isMessageLength && <p>Minimum length required is 4</p>}
         </div>
         <button type="submit">Submit</button>
       </form>
-      <div className="output">console.log(submission)</div>
+      <div className="output"></div>
     </div>
   );
 }

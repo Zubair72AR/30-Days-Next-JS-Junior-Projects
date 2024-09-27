86cfa4e3f07e5e04e9a6e3f880f18f59bdea70bd
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-09-27 13:09:07 +0500

    updated

--- day10-form-input-handling/src/components/form.tsx
+++ day10-form-input-handling/src/components/form.tsx
@@ -1,12 +1,27 @@
+"use client";
+import { useState } from "react";
+
 export default function Form() {
+  let [name, setName] = useState("");
+  let [email, SetEmail] = useState("");
+  let [message, SetMessage] = useState("");
+  let handleForm = (e: any) => {
+    e.preventDefault();
+    console.log(name);
+  };
+
   return (
     <div>
       <form>
         <div>
-          <label>
+          <label onSubmit={handleForm}>
             Name <span>*</span>
           </label>
-          <input type="text" placeholder="Enter here" />
+          <input
+            type="text"
+            placeholder="Enter here"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div>
           <label>
@@ -20,7 +35,7 @@ export default function Form() {
           </label>
           <textarea placeholder="Enter here" rows={4} />
         </div>
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );

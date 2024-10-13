b64cca4d1f80859bac1e57aad6e2de02af208c38
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-10-13 15:21:29 +0500

    updated

---
+++ day13-api-routes-next-js/src/app/page.tsx
@@ -0,0 +1,11 @@
+// pages/index.tsx
+import FetchData from "@/components/FetchData";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <FetchData />
+    </div>
+  );
+}

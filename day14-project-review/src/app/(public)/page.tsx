6eca6a577e1028d048d014b7d8b3717f6804ee0a
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-10-14 19:51:59 +0500

    updated

--- day14-project-review/src/app/(public)/page.tsx
+++ day14-project-review/src/app/(public)/page.tsx
@@ -1,8 +1,6 @@
-import NavBar from "../../components/NavBar";
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <h1>Hallo</h1>
     </div>
   );
--- day14-project-review/src/components/NavBar.tsx
+++ day14-project-review/src/components/NavBar.tsx
@@ -1,25 +1,46 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
+import { LuLogIn } from "react-icons/lu";
 
 let navLinks = [
   { id: 1, name: "Home", path: "/" },
-  { id: 1, name: "About Next JS", path: "/about" },
-  { id: 1, name: "Career", path: "/career" },
-  { id: 1, name: "Login", path: "/login" },
+  { id: 2, name: "Next JS", path: "/about" },
+  { id: 3, name: "Career", path: "/career" },
 ];
 
 export default function NavBar() {
+  let usePath = usePathname();
+  let isActive = (path: string) => path === usePath;
+
   return (
-    <nav className="flex justify-between items-center mx-8 md:mx-36 ">
-      <h1>Next JS</h1>
+    <nav className="flex justify-between items-center px-8 md:px-36 py-6  bg-slate-900 text-white">
+      <Link href="/">
+        <Image
+          className="dark:invert"
+          src="https://nextjs.org/icons/next.svg"
+          alt="Next.js logo"
+          width={160}
+          height={38}
+        />
+      </Link>
       <ul className="flex justify-center items-center gap-6">
         {navLinks.map((item) => (
           <li key={item.id}>
-            <Link href={item.path}>{item.name}</Link>
+            <Link
+              href={item.path}
+              className={isActive(item.path) ? "active" : "navClass"}
+            >
+              {item.name}
+            </Link>
           </li>
         ))}
       </ul>
+      <Link href="/login" className="flex justify-center items-center gap-2">
+        <LuLogIn />
+        Log In
+      </Link>
     </nav>
   );
 }
